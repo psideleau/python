b36c44a56037fcf6a21385bd7a2dc189ca869e86
psideleau <paulsideleau@yahoo.com>
2016-09-19 20:44:08 -0400

    Update test.py
    test

--- test/test.py
+++ test/test.py
@@ -7,7 +7,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertFalse('F2o'.isupper())
 
     def test_split(self):
         s = 'hello world'
